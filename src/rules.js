f55f6f5770a1137ae525a2a2badde5a9359da927
rhkdlfla <hy031225@kaist.ac.kr>
2026-01-31 17:20:38 +0900

    규칙 20까지 템플릿

--- src/rules.js
+++ src/rules.js
@@ -19,4 +19,89 @@ export const rules = [
         return sum === 25;
       },
     },
+    {
+        id: 4,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 5,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 6,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 7,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 8,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 9,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 10,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 11,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 12,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 13,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 14,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 15,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 16,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 17,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 18,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 19,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
+    {
+        id: 20,
+        desc: "규칙 설명",
+        check: (pw) => true,
+    },
   ];
