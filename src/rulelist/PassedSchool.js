a0cd110bdb8cdf752b9b21b4b87518acec4e3b7f
rhkdlfla <hy031225@kaist.ac.kr>
2026-02-03 13:57:41 +0900

    fix: ㅋ판정과 harvard 판정 추가

--- src/rulelist/PassedSchool.js
+++ src/rulelist/PassedSchool.js
@@ -1,5 +1,5 @@
 export const checkPassedSchool = (pw) => {
-    return pw.includes('KAIST') || pw.includes('카이스트') || pw.includes('하버드') || pw.includes('Harvard') || pw.includes('MIT') || pw.includes('서울과학고등학교');
+    return pw.includes('KAIST') || pw.includes('카이스트') || pw.includes('하버드') || pw.includes('Harvard') || pw.includes('harvard') || pw.includes('서울과학고등학교');
 };
 
 export const descriptionPassedSchool = "비밀번호에는 류석영 교수님이 거쳐간 학교 이름이 포함되어야 합니다.";
--- src/rulelist/Serious.js
+++ src/rulelist/Serious.js
@@ -1,5 +1,10 @@
+import Hangul from 'hangul-js';
+
 export const checkSerious = (pw) => {
-    return !pw.includes('K') && !pw.includes('ㅋ');
+    if (!pw) return true;
+    if (pw.includes('K') || pw.includes('k')) return false;
+    const disassembled = Hangul.disassemble(pw);
+    return !disassembled.some(jamo => jamo === 'ㅋ');
 };
 
 export const descriptionSerious = "이 비밀번호는 진지합니다… ㅋ이나 K을 포함하지 마세요";
